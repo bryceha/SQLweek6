b75bc65c8bb5f416223df41ca567b2aa42864994
bryceha <bryce.almstrum@gmail.com>
2019-08-25 17:29:20 -0700

    first commit

--- musicDB.sql
+++ musicDB.sql
@@ -1,15 +1,33 @@
-DELIMITER $$
-DROP PROCEDURE IF EXISTS SalariesAndHireDates $$
-CREATE PROCEDURE SalariesAndHireDates(
-    IN GivenSalary INT,
-    IN HireDate DATE)
-BEGIN
-    SELECT CONCAT(e.first_name, e.last_name) 'Name',
-        e.emp_no 'Emp. No',
-        e.hire_date 'Hire Date'
-    FROM employees e
-    INNER JOIN salaries s ON s.emp_no = e.emp_no
-    WHERE e.hire_date = HireDate
-    AND s.salary >= GivenSalary;
-END $$
-DELIMITER ;
+CREATE DATABASE IF NOT EXISTS music;
+
+USE music;
+
+DROP TABLE IF EXISTS songs;
+DROP TABLE IF EXISTS albums;
+DROP TABLE IF EXISTS artists;
+
+CREATE TABLE artists (
+    artist_name VARCHAR(255) NOT NULL,
+    formation_year INT(4),
+    PRIMARY KEY(artist_name)
+);
+
+CREATE TABLE albums (
+    album_id INT(4) NOT NULL auto_increment,
+    album_name VARCHAR(255) NOT NULL,
+    release_date DATE,
+    genre VARCHAR(255),
+    artist_name VARCHAR(255) NOT NULL,
+    PRIMARY KEY(album_id),
+    FOREIGN KEY(artist_name) REFERENCES artists(artist_name)
+); 
+
+CREATE TABLE songs (
+    song_id INT(6) NOT NULL auto_increment,
+    song_name VARCHAR(255) NOT NULL,
+    album_id INT(4),
+    artist_name VARCHAR(255) NOT NULL,
+    PRIMARY KEY(song_id),
+    FOREIGN KEY(album_id) REFERENCES albums(album_id),
+    FOREIGN KEY(artist_name) REFERENCES artists(artist_name)
+);
